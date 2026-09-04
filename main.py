9fa63b4a1299320c96ecb31d925358ea744d1ea8
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-09-04 18:50:15 +0700

    feat: add logging configuration

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 GOOGLE_SHEET_NAME = "Jimmy's expense log"
 SERVICE_ACCOUNT_FILE = "service_account.json"  
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="app.log",
+    filemode="a",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+logger = logging.getLogger(__name__)
